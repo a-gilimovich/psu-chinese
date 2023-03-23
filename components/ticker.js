4886d6af221b1561d70b214bc38ef4e938bec810
a-gilimovich <a.gilimovich@psu.by>
2023-03-23 11:46:18 +0300

    1

--- components/ticker.js
+++ components/ticker.js
@@ -3,14 +3,14 @@ import React from 'react';
 const Ticker = () => {
     return (
         <div className={"ticker"}>
-            <div>以耶夫罗西尼波洛茨克命名的波洛茨克国立大学&ensp;</div>
-            <div>以耶夫罗西尼波洛茨克命名的波洛茨克国立大学&ensp;</div>
-            <div>以耶夫罗西尼波洛茨克命名的波洛茨克国立大学&ensp;</div>
-            <div>以耶夫罗西尼波洛茨克命名的波洛茨克国立大学&ensp;</div>
-            <div>以耶夫罗西尼波洛茨克命名的波洛茨克国立大学&ensp;</div>
-            <div>以耶夫罗西尼波洛茨克命名的波洛茨克国立大学&ensp;</div>
-            <div>以耶夫罗西尼波洛茨克命名的波洛茨克国立大学&ensp;</div>
-            <div>以耶夫罗西尼波洛茨克命名的波洛茨克国立大学&ensp;</div>
+            <div>Euphrosyne Polotskaya State University of Polotsk&ensp;</div>
+            <div>Euphrosyne Polotskaya State University of Polotsk&ensp;</div>
+            <div>Euphrosyne Polotskaya State University of Polotsk&ensp;</div>
+            <div>Euphrosyne Polotskaya State University of Polotsk&ensp;</div>
+            <div>Euphrosyne Polotskaya State University of Polotsk&ensp;</div>
+            <div>Euphrosyne Polotskaya State University of Polotsk&ensp;</div>
+            <div>Euphrosyne Polotskaya State University of Polotsk&ensp;</div>
+            <div>Euphrosyne Polotskaya State University of Polotsk&ensp;</div>
         </div>
     );
 };
